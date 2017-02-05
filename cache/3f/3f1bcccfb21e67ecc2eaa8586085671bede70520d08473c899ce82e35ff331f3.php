8eded7bf512c9f8340e486939ee119317a66729f
Scott Stamp <Scott Stamp>
2017-02-04 20:31:55 -0330

    initial commit

---
+++ cache/3f/3f1bcccfb21e67ecc2eaa8586085671bede70520d08473c899ce82e35ff331f3.php
@@ -0,0 +1,86 @@
+<?php
+
+/* nav_common.html */
+class __TwigTemplate_b95fedaf960d400181f1714b63d0f4d9fd66d04df3f3b2e7df59e76798ad1f0c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "\t<nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">
+\t\t<div class=\"container\">
+\t\t\t<div class=\"navbar-header\">
+\t\t\t\t<button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar-collapse\">
+\t\t\t\t\t<span class=\"sr-only\">Toggle navigation</span>
+\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t</button>
+\t\t\t\t<a class=\"navbar-brand\" href=\"index.html\">Bob The Builder</a>
+\t\t\t</div>
+\t\t\t<div class=\"collapse navbar-collapse\" id=\"navbar-collapse\">
+\t\t\t\t<ul class=\"nav navbar-nav navbar-right\">
+\t\t\t\t\t<li>
+\t\t\t\t\t\t<a href=\"about.html\">About</a>
+\t\t\t\t\t</li>
+\t\t\t\t\t<li>
+\t\t\t\t\t\t<a href=\"services.html\">Services</a>
+\t\t\t\t\t</li>
+\t\t\t\t\t<li>
+\t\t\t\t\t\t<a href=\"contact.html\">Contact</a>
+\t\t\t\t\t</li>
+\t\t\t\t</ul>
+\t\t\t</div>
+\t\t</div>
+\t</nav>";
+    }
+
+    public function getTemplateName()
+    {
+        return "nav_common.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("\t<nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">
+\t\t<div class=\"container\">
+\t\t\t<div class=\"navbar-header\">
+\t\t\t\t<button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar-collapse\">
+\t\t\t\t\t<span class=\"sr-only\">Toggle navigation</span>
+\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t</button>
+\t\t\t\t<a class=\"navbar-brand\" href=\"index.html\">Bob The Builder</a>
+\t\t\t</div>
+\t\t\t<div class=\"collapse navbar-collapse\" id=\"navbar-collapse\">
+\t\t\t\t<ul class=\"nav navbar-nav navbar-right\">
+\t\t\t\t\t<li>
+\t\t\t\t\t\t<a href=\"about.html\">About</a>
+\t\t\t\t\t</li>
+\t\t\t\t\t<li>
+\t\t\t\t\t\t<a href=\"services.html\">Services</a>
+\t\t\t\t\t</li>
+\t\t\t\t\t<li>
+\t\t\t\t\t\t<a href=\"contact.html\">Contact</a>
+\t\t\t\t\t</li>
+\t\t\t\t</ul>
+\t\t\t</div>
+\t\t</div>
+\t</nav>", "nav_common.html", "C:\\xampp\\htdocs\\bobthebuilder\\templates\\nav_common.html");
+    }
+}
---
+++ cache/70/704105ddb4b2a1c62690ab3646191c6b1d10f513c302e1625a1c46196f2268f1.php
@@ -0,0 +1,88 @@
+<?php
+
+/* head_common.html */
+class __TwigTemplate_0eae28c183cbbdf426a737b822b2034706f636e60143f36486dd444d0c0e2510 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<head>
+
+    <meta charset=\"utf-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <meta name=\"description\" content=\"\">
+    <meta name=\"author\" content=\"\">
+
+    <title>Bob The Builder - Can We Fix It?</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">
+
+    <!-- Custom CSS -->
+    <link href=\"css/modern-business.css\" rel=\"stylesheet\">
+
+    <!-- Custom Fonts -->
+    <link href=\"font-awesome/css/font-awesome.min.css\" rel=\"stylesheet\" type=\"text/css\">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src=\"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script>
+        <script src=\"https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js\"></script>
+    <![endif]-->
+
+</head>";
+    }
+
+    public function getTemplateName()
+    {
+        return "head_common.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<head>
+
+    <meta charset=\"utf-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <meta name=\"description\" content=\"\">
+    <meta name=\"author\" content=\"\">
+
+    <title>Bob The Builder - Can We Fix It?</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">
+
+    <!-- Custom CSS -->
+    <link href=\"css/modern-business.css\" rel=\"stylesheet\">
+
+    <!-- Custom Fonts -->
+    <link href=\"font-awesome/css/font-awesome.min.css\" rel=\"stylesheet\" type=\"text/css\">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src=\"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script>
+        <script src=\"https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js\"></script>
+    <![endif]-->
+
+</head>", "head_common.html", "C:\\xampp\\htdocs\\bobthebuilder\\templates\\head_common.html");
+    }
+}
